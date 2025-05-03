246ee5d67ceed8ee997d877b5afdfa1ec6c6d659
El-Sankey <i.a.rivashernandez@gmail.com>
2025-05-03 20:05:39 +0200

    1ste commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import './App.css';
+import {inventory} from './constants/inventory.js';
+
+
 
 function App() {
-  return (
-    <h1>Begin hier met met maken van de applicatie!</h1>
-  )
+function soldTv() {
+    const soldTv = inventory[0].sold;
+    soldTv.map(inventory[0].sold.length)
+    console.log(soldTv)
+    return 'Er zijn ' + (soldTv) + 'tv\'s verkocht'
+}
+soldTv()
+    return (
+        <>
+            <h1>Tech it easy dashboard</h1>
+            <p>Verkocht overzicht</p>
+            <div>Aantal verkochte producten</div>
+            {soldTv()}
+        </>
+)
+
 }
 
 export default App
+
+
+
+
